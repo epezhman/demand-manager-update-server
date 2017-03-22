515279a3a864dcc8ba3fd9c7b3034f281e9e6dce
Pezhman Nasirifard <epezhman@gmail.com>
2017-03-22 18:01:26 +0100

    update to last version

--- index.js
+++ index.js
@@ -3,34 +3,13 @@ const express = require('express');
 const app = express();
 const request = require('request');
 
-var versionOSX;
 var versionLinux32;
 var versionLinux64;
 
-const FETCH_INTERVAL_OSX = 300000;
 const FETCH_INTERVAL_LINUX64 = 305000;
 const FETCH_INTERVAL_LINUX32 = 310000;
 
 app.use(require('morgan')('dev'));
-
-
-app.get('/updates/latest/osx', (req, res) => {
-    if (versionOSX) {
-        const clientVersion = req.query.v;
-
-        if (clientVersion === versionOSX) {
-            res.status(204).end();
-        } else {
-            res.json({
-                url: `${getBaseUrl()}updates/latest/osx/TUM Demand Manager-${versionOSX}-mac.zip`
-            });
-        }
-    }
-    else {
-        res.status(204).end();
-    }
-});
-
 
 app.get('/updates/latest/linux64', (req, res) => {
     if (versionLinux64) {
@@ -40,7 +19,7 @@ app.get('/updates/latest/linux64', (req, res) => {
             res.status(204).end();
         } else {
             res.json({
-                url: `${getBaseUrl()}updates/latest/linux64/tumdmdesktop-${versionLinux64}.deb`,
+                url: `${getBaseUrl()}updates/latest/linux64/i13dmdesktop_${versionLinux64}_amd64.deb`,
                 file: `tumdmdesktop-${versionLinux32}.deb`
             });
         }
@@ -59,7 +38,7 @@ app.get('/updates/latest/linux32', (req, res) => {
             res.status(204).end();
         } else {
             res.json({
-                url: `${getBaseUrl()}updates/latest/linux64/tumdmdesktop-${versionLinux32}-ia32.deb`,
+                url: `${getBaseUrl()}updates/latest/linux64/i13dmdesktop_${versionLinux32}_ia32.deb`,
                 file: `tumdmdesktop-${versionLinux32}-ia32.deb`
             });
         }
@@ -71,21 +50,6 @@ app.get('/updates/latest/linux32', (req, res) => {
 
 let getBaseUrl = () => {
     return 'https://s3.eu-central-1.amazonaws.com/demand-manager-resources/'
-}
-
-let getVersionForOSX = () => {
-    console.log(`Fetching latest version from ${versionOSXUrl}`);
-    request.get(versionOSXUrl, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            versionOSX = body;
-            console.log(versionOSX)
-        }
-        else if (error) {
-            console.error(error);
-        }
-    });
-
-    setTimeout(getVersionForOSX, FETCH_INTERVAL_OSX);
 }
 
 let getVersionForLinux64 = () => {
@@ -116,13 +80,11 @@ let getVersionForLinux32 = () => {
     });
 
     setTimeout(getVersionForLinux32, FETCH_INTERVAL_LINUX32);
-}
+};
 
 
-const versionOSXUrl = `${getBaseUrl()}updates/latest/osx/buildVersion`;
 const versionLinux64Url = `${getBaseUrl()}updates/latest/linux64/buildVersion`;
 const versionLinux32Url = `${getBaseUrl()}updates/latest/linux32/buildVersion`;
-getVersionForOSX();
 getVersionForLinux64();
 getVersionForLinux32();
 
